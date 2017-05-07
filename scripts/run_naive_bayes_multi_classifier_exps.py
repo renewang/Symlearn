53a5be739ebf9ee7945f83f65d2db5108d06c441
renewang <renewang@users.noreply.github.com>
2017-05-07 10:57:44 +0800

    simplify requirements.txt

--- scripts/run_naive_bayes_multi_classifier_exps.py
+++ scripts/run_naive_bayes_multi_classifier_exps.py
@@ -15,7 +15,8 @@ if __name__ == '__main__':
     import joblib
 
     timeout_ = None
-    exp_leves = numpy.logspace(0, 2, 3, base=4, dtype=numpy.int)
+    exp_levels = numpy.logspace(0, 2, 3, base=4, dtype=numpy.int)
+    print(numpy.array2string(exp_levels))
     with concurrent.futures.ProcessPoolExecutor(max_workers=None) as executor:
         exec_res = [executor.submit(run_multi_classifiers, csvfile, cvkws, gridkws,
                     predefine=True, n_rows=-1, use_ensemble=False,
@@ -33,7 +34,7 @@ if __name__ == '__main__':
                     random_state=None, max_level=i, batch_mode=True) for i in exp_levels]
         exec_ens = []
         for future in concurrent.futures.as_completed(exec_res):
-            print("decision tree multi-classifier worker status:", future.done(), "test accruacy:", future.result()[-1])
+            print("ensemble multi-classifier worker status:", future.done(), "test accruacy:", future.result()[-1])
             exec_ens.append(future.result())
     concurrent.futures.wait(exec_res, timeout=timeout_, return_when=ALL_COMPLETED)
     joblib.dump((exec_dec, exec_ens), os.path.join(data_dir, 'multi_classifier_exps.pkl'))
